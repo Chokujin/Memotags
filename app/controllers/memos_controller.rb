54cb715708f598d00b8fbdd5656a7eb927cd16fe
naoto.sasaki <kukandenpan@gmail.com>
2019-06-16 12:49:41 +0000

    memo index

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -3,6 +3,15 @@ class MemosController < ApplicationController
   end
 
   def create
+    @memo = current_user.memos.build(memo_params)
+    if @memo.save
+      flash[:success] = 'タスクを登録しました。'
+      redirect_to root_url
+    else
+      @memos = current_user.memos.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'タスクの登録に失敗しました。'
+      render 'toppages/index'
+    end
   end
 
   def index
@@ -18,9 +27,30 @@ class MemosController < ApplicationController
   end
 
   def update
+    if @memo.update(memo_params)
+      flash[:success] = 'メモは正常に更新されました'
+      redirect_to @memo
+    else
+      flash.now[:danger] = 'メモは更新されませんでした'
+      render :edit
+    end
   end
   
   def calender
+  end
+  
+  private
+  
+  def memo_params
+    params.require(:memo).permit(:content)
+  end
+
+  
+  def correct_user
+    @memo = current_user.memos.find_by(id: params[:id])
+    unless @memo
+      redirect_to root_url
+    end
   end
   
 end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,6 @@
 class ToppagesController < ApplicationController
   def index
+      @memo = current_user.memos.build
+      @memos = current_user.memos.order(id: :desc).page(params[:page])
   end
 end
